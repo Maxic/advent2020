5904832d401944ec4ea5dc2bdc34908c7c96a19f
Barend <barend.poot@gmail.com>
2020-12-05 17:36:30 +0100

    Fixed bug in passport validation

--- day4/passport.py
+++ day4/passport.py
@@ -1,5 +1,6 @@
 import re
 
+
 class Passport:
     def __init__(self, data):
         self.data = data
@@ -72,18 +73,24 @@ class Passport:
     def validate_proper(self):
 
         # Birth year validation
+        if self.byr.__len__() != 4:
+            return False
         if not re.search(r'^(?:[0-9]{4})$', self.byr):
             return False
         if int(self.byr) < 1920 or int(self.byr) > 2002:
             return False
 
         # Issue year validation
+        if self.iyr.__len__() != 4:
+            return False
         if not re.search(r'^(?:[0-9]{4})$', self.iyr):
             return False
         if int(self.iyr) < 2010 or int(self.iyr) > 2020:
             return False
 
         # Expiration Year validation
+        if self.eyr.__len__() != 4:
+            return False
         if not re.search(r'^(?:[0-9]{4})$', self.eyr):
             return False
         if int(self.eyr) < 2020 or int(self.eyr) > 2030:
@@ -94,9 +101,13 @@ class Passport:
             return False
         else:
             if self.hgt.endswith('cm'):
+                if self.hgt.__len__() != 5:
+                    return False
                 if int(self.hgt[:3]) < 150 or int(self.hgt[:3]) > 193:
                     return False
             if self.hgt.endswith('in'):
+                if self.hgt.__len__() != 4:
+                    return False
                 if int(self.hgt[:2]) < 59 or int(self.hgt[:2]) > 76:
                     return False
 
@@ -105,15 +116,15 @@ class Passport:
             return False
 
         # Eye color validation
-        if self.ecl == '':
-            return False
         if self.ecl not in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']:
             return False
 
         # Passport ID validation
         if self.pid == '':
             return False
-        if not (self.pid.isdigit() and self.pid.lstrip('0').__len__() <= 9):
+        if self.pid.__len__() != 9:
+            return False
+        if not (self.pid.isdigit()):
             return False
 
         # Country ID validation
